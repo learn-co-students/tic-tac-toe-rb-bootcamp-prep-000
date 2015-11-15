a55bfa4a3a12334e478433cee958deb1cb9746f1
aviflombaum <avi@flombaum.com>
2015-11-15 15:25:36 -0500

    add include_array matcher for win_combo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,12 @@ RSpec.configure do |config|
   config.order = :default
 end
 
+RSpec::Matchers.define :include_array do |expected|
+  match do |actual|
+    actual.any?{|array| match_array(expected).matches?(array)}
+  end
+end
+
 def run_file(file)
   eval(File.read(file), binding)
 end
@@ -15,12 +21,12 @@ end
 def get_variable_from_file(file, variable)
   file_scope = binding
   file_scope.eval(File.read(file))
-  
+
   begin
     return file_scope.local_variable_get(variable)
   rescue NameError
     raise NameError, "local variable `#{variable}' not defined in #{file}."
-  end    
+  end
 end
 
 def capture_puts
