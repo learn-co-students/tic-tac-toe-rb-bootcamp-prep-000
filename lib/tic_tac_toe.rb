5b0c0c5932b695b371e41a7f446ad4f0a2435b0b
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-04-25 10:50:59 -0600

    Test move method to make sure it requires 3 arguments
    Add tests to ensure that the move method no longer works with a
    default third argument. This was causing problems in the test and
    the default argument was never used anyway.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,7 +76,7 @@ def turn_count(board)
   board.count{|token| token == "X" || token == "O"}
 end
 
-def move(board, location, player = "X")
+def move(board, location, player)
   board[location.to_i-1] = player
 end
 
