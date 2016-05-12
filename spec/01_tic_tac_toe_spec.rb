663016eb706eb5c07e4c1fc47cb1444744239ece
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-05-12 15:41:52 -0400

    Update readme and spec so position is 1-9 and index is 0-8

--- spec/01_tic_tac_toe_spec.rb
+++ spec/01_tic_tac_toe_spec.rb
@@ -41,24 +41,24 @@ describe './lib/tic_tac_toe.rb' do
     end
   end
 
-  describe '#input_to_position' do
+  describe '#input_to_index' do
 
     it 'convervets a user_input to an integer' do
       user_input = "1"
 
-      expect(input_to_position(user_input)).to be_a(Integer)
+      expect(input_to_index(user_input)).to be_a(Integer)
     end
 
     it 'subtracts 1 from the user_input' do
       user_input = "6"
 
-      expect(input_to_position(user_input)).to be(5)
+      expect(input_to_index(user_input)).to be(5)
     end
 
     it 'returns -1 for strings without integers' do
       user_input = "invalid"
 
-      expect(input_to_position(user_input)).to be(-1)
+      expect(input_to_index(user_input)).to be(-1)
     end
 
   end
