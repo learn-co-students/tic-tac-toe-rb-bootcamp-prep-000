9e6dc69a516a992bf4554ac517d0f4203e5592bd
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-05-12 15:42:33 -0400

    Update spec to not ref position taken

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,7 +59,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-  index = input_to_position(user_input)
+  index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, current_player(board))
     display_board(board)
