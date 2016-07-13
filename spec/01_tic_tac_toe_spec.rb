3cc5b9b4dd7f00e9e15ab4e5d5c3158268d359b9
gj <gj@users.noreply.github.com>
2016-07-13 13:51:44 -0400

    convervets --> converts

--- spec/01_tic_tac_toe_spec.rb
+++ spec/01_tic_tac_toe_spec.rb
@@ -43,7 +43,7 @@ describe './lib/tic_tac_toe.rb' do
 
   describe '#input_to_index' do
 
-    it 'convervets a user_input to an integer' do
+    it 'converts a user_input to an integer' do
       user_input = "1"
 
       expect(input_to_index(user_input)).to be_a(Integer)
