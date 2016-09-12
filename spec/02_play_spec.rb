890986c518dfaf5ebdc4a4f556dbb0ebf5b7a88c
Hadeel Alharthi <hadeel.alharthi@hotmail.com>
2016-09-12 20:25:06 +0300

    Done.

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/tic_tac_toe.rb'
 
-describe './lib/tic_tac_toe.rb' do  
+describe './lib/tic_tac_toe.rb' do
   describe '#play' do
     it 'asks for players input on a turn of the game' do
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
