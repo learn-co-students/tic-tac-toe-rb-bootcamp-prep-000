eb5d3291186ea5cd63442da969c0d8f2ee360a8f
Denis C <dvc.838@gmail.com>
2016-09-12 23:36:50 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,89 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  return board.count("X") + board.count("O")
+end
+
+def current_player(board)
+  return turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |c|
+    if c.all? { |value| board[value] == "X" } ||
+      c.all? { |value| board[value] == "O" }
+      return c
+    end
+  end
+  return false
+end
+
+def full?(board)
+  return board.none? { |cell| cell == " "}
+end
+
+def draw?(board)
+  return !won?(board) && full?(board)
+end
+
+def over?(board)
+  return won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  winner = won?(board) and return board[winner[0]]
+  return nil
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+
+  puts won?(board) ? "Congratulations #{winner(board)}!" : "Cats Game!"
+end
