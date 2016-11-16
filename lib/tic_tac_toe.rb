789c5b8fd4d736c33e29dfb2c3e391bfc32c97e4
Prasenjit Banik <pbp2005@gmail.com>
2016-11-16 02:21:42 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,101 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  idx = gets.strip.to_i
+  input = input_to_index(idx)
+  until valid_move?(board, input)
+    puts "Please enter 1-9:"
+    idx = gets.strip.to_i
+    input = input_to_index(idx)
+  end
+  move(board, input, current_player(board))
+  display_board(board)
+end
+
+# Define your play method below
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end
+
+def turn_count(board)
+  counter = 0
+    board.each do |i|
+      if i != " "
+      counter += 1
+      end
+    end
+    return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+WIN_COMBINATIONS = [
+                   [0,1,2],
+                   [3,4,5],
+                   [6,7,8],
+                   [0,3,6],
+                   [1,4,7],
+                   [2,5,8],
+                   [0,4,8],
+                   [2,4,6]
+                 ]
+def won?(board)
+  WIN_COMBINATIONS.detect do |i|
+    board[i[0]] == board[i[1]] && board[i[1]] == board[i[2]] && position_taken?(board,i[0])
+  end
+end
+
+def full?(board)
+  board.all?{|i| i == "X" || i == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def winner(board)
+  if win_combination = won?(board)
+    board[win_combination.first]
+  end
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
