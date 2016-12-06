ff25c29e3ecf53cac225c0fb21da286585a301c2
Paul Snider <pbsnider@gmail.com>
2016-12-06 23:59:35 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,14 +19,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
 def input_to_index(user_input)
   user_input.to_i - 1
 end
@@ -55,8 +47,6 @@ def turn(board)
     turn(board)
   end
 end
-
-
 
 def turn_count(board)
   counter = 0
