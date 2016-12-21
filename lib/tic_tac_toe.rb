2a67ce6b2173eccd0687f224eb7ec3126926b58b
Devin Rieger <dev0089@gmail.com>
2016-12-21 22:56:41 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,113 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def play(board)
+
+  until over?(board)
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    player = current_player(board)
+    move(board, index, player)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |turns|
+    if turns == "X" || turns == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect{|combo|
+    combo.all?{|index| position_taken?(board, index)} and
+    (combo.all?{|index| board[index] == 'X'} or
+    combo.all?{|index| board[index] == 'O'})
+  }
+end
+
+def full?(board)
+  board.all?{|index| !(index.nil? || index == " ")}
+end
+
+def draw?(board)
+  if !(!!won?(board)) && full?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if draw?(board) || !!won?(board) || full?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if over?(board)
+    board[won?(board)[0]]
+  end
+end
