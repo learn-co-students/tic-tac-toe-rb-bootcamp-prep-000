904d481e7e19c0496e99e4c0589b9f1c9205ba52
Devin Oulighan <doulighan@gmail.com>
2017-02-07 19:33:30 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,102 @@
+# Helper Methods
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+# ////////////////////////// GAME STATUS//////////////////////////////
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combination|
+    board[combination[0]] == board[combination[1]] &&
+    board[combination[1]] == board[combination[2]] &&
+    position_taken?(board, combination[0])
+  end
+end
+
+def full?(board)
+  all_full = board.all? do |space|
+    space == "X" || space  == "O"
+  end
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  full?(board) || won?(board) || draw?(board)
+end
+
+def winner(board)
+  if winnerArray = won?(board)
+    board[winnerArray[0]]
+  end
+end
+
+# ////////////////////////// CURRRENT PLAYER/TURN COUNT //////////////////////////////
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "O" || space == "X"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
