d83e4dceb51a8b9f45531ca9ac05e83dc84b2c55
Vagelis Kalimeris <aka9080@yahoo.gr>
2017-02-28 22:49:00 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,119 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |spot|
+    if spot == "X" || spot == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  times = turn_count(board)
+  if times % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    if combination.all? do |index|
+        board[index] == "X"
+      end
+      return combination
+    elsif combination.all? do |index|
+        board[index] == "O"
+      end
+      return combination
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.each do |position|
+    if position == " " || position.nil?
+      return false
+    end
+  end
+  return true
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  win_array = won?(board)
+  if win_array == false
+    return nil
+  elsif board[win_array[0]] == "X"
+    return "X"
+  elsif board[win_array[0]] == "O"
+    return "O"
+  end
+end
+
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if winner(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end
