2ee9976d43094ddf8ae06820d7798f276282e897
Tayeb Mc <gomail654@gmail.com>
2017-03-05 18:34:34 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,66 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
+def display_board(board)
+puts    (" #{board[0]} | #{board[1]} | #{board[2]} ")
+puts    ("-----------")
+puts    (" #{board[3]} | #{board[4]} | #{board[5]} ")
+puts    ("-----------")
+puts    (" #{board[6]} | #{board[7]} | #{board[8]} ")
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != "X" && board[location] != "O"
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+# Define your play method below
+def play(board)
+  while !over?(board)
+      turn(board)
+  end
+   if won?(board)
+     puts"Congratulatins #{winner(board)}"
+   elsif draw?(board)
+     puts"Cats Game!"
+   end
+ end
