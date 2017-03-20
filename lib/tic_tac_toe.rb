ec878094413af24e0c3901c89eb76b1e61d885cf
Aurelie Felsen <aurelie.felsen@gmail.com>
2017-03-20 19:45:25 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,99 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6]
+  ]
+
+def input_to_index(input)
+  index = input.to_i-1
+end
+
+def move(board, index, character)
+  board[index] = character
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+count = 0
+board.each do |move|
+  if move == "X" || move == "O"
+  count += 1
+end
+end
+count
+end
+
+def current_player(board)
+turn_count(board).even? ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+board.all?{|cell| cell == "X" || cell == "O"}? true: false
+end
+
+def draw?(board)
+!won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
+
+def play(board)
+  while !over?(board)
+     turn(board)
+  end
+  if won?(board)
+     puts "Congratulations #{winner(board)}!"
+   elsif draw?(board)
+    puts "Cats Game!"
+   end
+ end
