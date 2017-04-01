8adcf0ab81f448fdf2e43026f8dff91da1257541
Michael Finlay <finlayma84@gmail.com>
2017-04-01 20:02:58 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,104 @@
+WIN_COMBINATIONS=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+     puts " #{board[0]} | #{board[1]} | #{board[2]} "
+     puts "-----------"
+     puts " #{board[3]} | #{board[4]} | #{board[5]} "
+     puts "-----------"
+     puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i
+  index - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board,index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+def turn_count(board)
+  counter = 0
+    board.each do |turn|
+      if turn == "X" || turn == "O"
+      counter += 1
+    end
+    end
+ counter
+end
+
+def current_player(board)
+  value=turn_count(board)
+  if value % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+def won?(board)
+    WIN_COMBINATIONS.detect do |wincombo|
+    board[wincombo[0]] == board[wincombo[1]] && board[wincombo[1]] == board[wincombo[2]] &&
+      position_taken?(board, wincombo[0])
+    end
+end
+
+def full?(board)
+  board.all? do |move|
+    move=="X"||move=="O"
+  end
+end
+
+def draw?(board)
+  !won?(board)&&full?(board)
+end
+
+def over?(board)
+  draw?(board)||won?(board)
+end
+
+def winner(board)
+  if combo=won?(board)
+board[combo.first]
+  end
+ end
+
+
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+     if won?(board)
+       puts "Congratulations #{winner(board)}!"
+     elsif draw?(board)
+      puts "Cats Game!"
+   end
+ end
