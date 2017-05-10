a41a32b1039de33c63d292f67ba0133cd42668f1
Pradeep Dayaram <vickdayaram@gmail.com>
2017-05-10 20:22:40 +0000

    Done.

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/tic_tac_toe.rb'
 
-describe './lib/tic_tac_toe.rb' do  
+describe './lib/tic_tac_toe.rb' do
   describe '#play' do
     it 'asks for players input on a turn of the game' do
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
@@ -39,7 +39,7 @@ describe './lib/tic_tac_toe.rb' do
       allow($stdout).to receive(:puts)
       allow(self).to receive(:gets).and_return("1","2","3")
       allow(self).to receive(:over?).and_return(false, false, false, true)
-      allow(self).to receive(:turn) do
+      allow(self).to receive(:turn)  do
         num_of_turns += 1
         Process.exit!(true) if num_of_turns > 10
       end.and_call_original
