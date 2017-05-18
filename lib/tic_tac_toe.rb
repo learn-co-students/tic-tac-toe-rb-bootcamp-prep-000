f9f32d460670501a70b47ce3952774c3878fcf55
Tony Nassif <tonynassif@gmail.com>
2017-05-18 19:54:42 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,101 @@
+# Helper Methods
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+ counter = 0
+ board.each do |turn|
+  if (turn == "X") || (turn == "O")
+    counter +=1
+  end
+ end
+ return counter
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], #Middle row
+  [6,7,8], #Bottom row
+  [0,3,6], #Left column
+  [1,4,7], #Middle column
+  [2,5,8], #Right column
+  [0,4,8], #Left start diagonal
+  [2,4,6]  #Right start diagonal
+]
+
+def won?(board)
+winner = []
+empty_board = board.all? { |x| x == " "}
+WIN_COMBINATIONS.each do |sub_array|
+    if empty_board
+      return false
+    elsif sub_array.all? { |value| board[value] =="X" } || sub_array.all? { |value| board[value] =="O" }
+      winner = sub_array
+    end
+  end
+  winner == [] ? false : winner
+end
+
+def full?(board)
+  !board.any? { |x| x == " " }
+end
+
+def draw?(board)
+  !won?(board) && full?(board) ? true : false
+end
+
+def over?(board)
+  draw?(board) || won?(board) ? true : false
+end
+
+def winner(board)
+  winning_board = won?(board)
+  winning_board ? board[winning_board[0]] : nil
+end
+
+=begin# Define your play method below
+def play(board)
+  i = 0
+  while i < 9
+    turn(board)
+    i +=1
+  end
+end
+=end
