5aa1d275579342aeb30015b1f0ee4fc9a93ccc9f
David Tenenbaum <david.tenenbaum87@gmail.com>
2017-06-29 19:07:58 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,109 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
 
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+    user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+    board[index] = current_player
+end
+
+def position_taken?(board, location)
+    board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+      if valid_move?(board, index)
+        move(board, index, current_player(board))
+        display_board(board)
+      else
+        turn(board)
+      end
+end
+
+def turn_count(board)
+    count = 0
+    board.each { |index| count += 1 if index == "X" || index == "O" }
+    return count
+end
+
+def current_player(board)
+    if turn_count(board) % 2 == 0
+      return "X"
+    else
+      return "O"
+    end
+end
+
+def won?(board)
+    WIN_COMBINATIONS.each do |win_comb|
+      position_1 = board[win_comb[0]]
+      position_2 = board[win_comb[1]]
+      position_3 = board[win_comb[2]]
+      return win_comb if (position_1 != " ") && ((position_1 == position_2) && (position_1 == position_3))
+    end
+    return false
+end
+
+def full?(board)
+    board.all? do |chr|
+      chr != " "
+    end
+end
+
+def draw?(board)
+    return true if !won?(board) && full?(board)
+end
+
+def over?(board)
+    if won?(board) || draw?(board) || full?(board)
+      return true
+    else
+      return false
+    end
+end
+
+def winner(board)
+    WIN_COMBINATIONS.each do |win_comb|
+      position_1 = board[win_comb[0]]
+      position_2 = board[win_comb[1]]
+      position_3 = board[win_comb[2]]
+      return position_1 if (position_1 != " ") && ((position_1 == position_2) && (position_1 == position_3))
+    end
+    return nil
+end
+
+def play(board)
+    until over?(board)
+      turn(board)
+    end
+    if won?(board)
+      puts "Congratulations #{winner(board)}!"
+    elsif draw?(board)
+      puts "Cats Game!"
+    end
+end
