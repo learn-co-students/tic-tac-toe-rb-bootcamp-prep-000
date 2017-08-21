f1705d995f7c34085dbaf3cd1c0cff73d618ede0
George gore9213@gmail.com <georgelgore@gmail.com>
2017-08-21 22:14:06 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
