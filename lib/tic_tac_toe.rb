fceb606188fe25eb158a5fe69f6df65727ab9f1f
Antonio Reid <read.musik@gmail.com>
2017-09-18 03:17:11 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,161 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+
+  row_one = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row_two = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row_three = " #{board[6]} | #{board[7]} | #{board[8]} "
+  line_separate = "-----------"
+  puts row_one
+  puts line_separate
+  puts row_two
+  puts line_separate
+  puts row_three
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, char)
+  board[index] = char
+end
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    true
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  char = current_player(board)
+  if valid_move?(board, index)
+    move(board, index, char)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |move|
+    if move == 'X' || move == 'O'
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do|combination|
+    win_index_1 = combination[0]
+    win_index_2 = combination[1]
+    win_index_3 = combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return combination
+    elsif  position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return combination
+    else
+      false
+    end
+  end
+  empty_board = board.all? do|position|
+    position == "X" || position == "O"
+end
+WIN_COMBINATIONS.include?(board)
+end
+
+def full?(board)
+
+  full_board = board.all? do|e|
+    e == "X" || e == "O"
+  end
+end
+
+def draw?(board)
+
+full?(board)
+if won?(board)
+  return false
+end
+  board.all? do|e|
+    e == "X" || e == "O"
+  end
+
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  end
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do|combination|
+    win_index_1 = combination[0]
+    win_index_2 = combination[1]
+    win_index_3 = combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+       return "X"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+       return "O"
+  end
+
+  end
+  return nil
+end
+
+def play(board)
+  counter = 0
+  while !over?(board)
+
+    turn(board)
+    counter += 1
+  end
+  if winner(board) == "X"
+    puts "Congratulations X!"
+  elsif winner(board) == "O"
+    puts "Congratulations O!"
+  elsif draw?(board) == true
+    puts "Cat's Game!"
+  end
+end
