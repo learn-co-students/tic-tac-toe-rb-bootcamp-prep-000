4baf2c1e3b6409962f1c1b56e9847b68cdcde420
Anthony Grimes <agrimes@charter.net>
2017-10-01 14:53:49 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,67 @@
+# define winning winning combinations
+WIN_COMBINATIONS = [
+   [0,1,2],
+   [3,4,5],
+   [6,7,8],
+   [0,3,6],
+   [1,4,7],
+   [2,5,8],
+   [0,4,8],
+   [6,4,2]
+]
 
+def play(board)
+   
+end
+
+def display_board(board)
+
+end
+
+def position_taken?(board, position)
+
+end
+
+def valid_move?(board, position)
+
+end
+
+def input_to_index(input)
+
+end
+
+def move(board, position, player)
+
+end
+
+def turn
+
+end
+
+def turn_count(board)
+
+end
+
+def won?(board)
+
+end
+
+def current_player(board)
+
+end
+
+def full?(board)
+
+end
+
+def draw?(board)
+
+end
+
+def over?(board)
+
+end
+
+def winner(board)
+
+end
