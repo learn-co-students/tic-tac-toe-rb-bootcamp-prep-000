4d43c00eb33eb8c5b0fbbb883fd2812cae07099c
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-24 18:49:45 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,129 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
+# Helper methods below
+def display_board(board)
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  separator = "-----------"
+
+  puts row1
+  puts separator
+  puts row2
+  puts separator
+  puts row3
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, token)
+  board[index] = token
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0, 8)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+end
+
+def turn_count(board)
+    turns = 0
+    board.each do |turn|
+      if turn != " "
+        turns += 1
+      end
+    end
+    turns
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win|
+    if win.all?{|i| board[i] == "X"}
+      return win
+    elsif win.all?{|i| board[i] == "O"}
+      return win
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.none? do |token|
+    if token == " "
+      true
+    else
+      false
+    end
+  end
+end
+
+def draw?(board)
+  if won?(board)
+    false
+  elsif full?(board)
+    true
+  end
+end
+
+def over?(board)
+  if draw?(board)
+    return true
+  elsif won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    WIN_COMBINATIONS.each do |win|
+      if win.all?{|i| board[i] == "X"}
+        return "X"
+      elsif win.all?{|i| board[i] == "O"}
+        return "O"
+      end
+    end
+  end
+  return nil
+end
+
+def play(board)
+  until over?(board)
+      turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat\'s Game!"
+  end
+end
