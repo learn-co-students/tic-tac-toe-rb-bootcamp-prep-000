611d5370bc61591ee1ca5cdceff347743219378f
Ronil Bhatia <ronilbhatia@gmail.com>
2017-10-26 04:10:22 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,145 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+
+
+
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+# Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do|win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    else
+      false
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? do |index|
+    index == "X" || index == "O"
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  if won?(board)
+  x_wins = won?(board)[0]
+    if board[x_wins] == "X"
+      return "X"
+    elsif board[x_wins] == "O"
+      return "O"
+    else
+      nil
+    end
+  else
+    nil
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do|position|
+    if position != " " && position != "" && position != nil
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_number = turn_count(board)
+  if turn_number%2 != 0
+    "O"
+  else
+    "X"
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  player = current_player(board)
+  if valid_move?(board, index)
+    move(board, index, player)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+  if !over?(board)
+    turn(board)
+    if won?(board)
+      player = winner(board)
+      puts "Congratulations #{player}!"
+    elsif draw?(board)
+      puts "Cat's Game!"
+    else
+      play(board)
+    end
+  elsif won?(board)
+    player = winner(board)
+    puts "Congratulations #{player}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
