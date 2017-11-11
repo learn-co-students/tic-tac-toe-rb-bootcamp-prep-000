d06a770cbf9c83397f3f64ac10ce00f0f141af2a
Liz Burton <liz.burton147@gmail.com>
2017-11-11 21:46:56 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,109 @@
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  current_player = current_player(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      count+=1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
+
+def won? (board)
+  WIN_COMBINATIONS.each do |index_array|
+    combo = []
+    index_array.each do |index|
+      combo << board[index]
+    end
+    if combo.all? { |move| move == "X" } || combo.all? { |move| move == "O" }
+      return index_array
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? do |element|
+    element == "X" || element == "O"
+  end
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    return true
+  end
+end
+
+def winner(board)
+  if won?(board)
+    puts "Congratulations #{board[won?(board)[0]]}!"
+    return board[won?(board)[0]]
+  else
+    puts "Cat's Game!"
+  end
+end
+
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  winner(board)
+end
