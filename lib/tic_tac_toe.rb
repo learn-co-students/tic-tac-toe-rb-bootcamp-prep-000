a252ef077e03f26356e7d97aae841a6d56c03b55
Karen Zhao <karen.zhao18@gmail.com>
2017-11-29 23:33:51 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,4 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-# Define your WIN_COMBINATIONS constant
+# DEFINE constant capitalized, 8 ways to win
 WIN_COMBINATIONS = [
   [0,1,2], # Top Row
   [3,4,5], # Middle
@@ -15,109 +10,170 @@ WIN_COMBINATIONS = [
   [2,4,6]  # Rising Diag
 ]
 
-def iter_win_combination?(board, win_com)
-  win_com.all? do |place|
-    board[place] == position_taken?(board,index)
+#rb
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts 'display_board'
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+
+
+
+
+def check_win_combination?(board, current_player, win_combo)
+  # win_combo.all? do |position|
+  #   board[position] == current_player
+  win_index_1 = win_combo[0]
+  win_index_2 = win_combo[1]
+  win_index_3 = win_combo[2]
+
+  position_1 = board[win_index_1] # load value of board at win_index_1
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+
+  if position_1 == current_player && position_2 == current_player && position_3 == current_player
+    return win_combo
   end
 end
 
+# WON?
 def won?(board)
-  WIN_COMBINATIONS.each do |win_com|
-    if iter_win_combination?(board, token, win_com)
-      win_comb
+  WIN_COMBINATIONS.each do |win_combo|
+    if check_win_combination?(board, 'X', win_combo)
+      return win_combo
+    elsif check_win_combination?(board, 'O', win_combo)
+      return win_combo
     else
-      win_comb
+      return false
     end
   end
 end
 
+
+
+
+
+
+
+# HELPER METHOD
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+# FULL?
 def full?(board)
   if board.include?(" ") || board.include?("")
-    false
+    return false
   else
-    true
+    return true
   end
 end
 
+# DRAW?
 def draw?(board)
-  if !won?(board)
-    true
-  else
-    false
+  if !won?(board) && full?(board)
+    return true
   end
 end
 
+# OVER?
 def over?(board)
   if won?(board) || draw?(board) || full?(board)
-    true
+    return true
   else
-    false
+    puts 'not over, keep going'
+    return false
   end
 end
 
+# WINNER
 def winner(board)
-  won?(board).token
-end
-
-
-def display_board
-
-end
-
-def input_to_index(user_input)
-
-end
-
-def move(board,index,token)
-
-end
-
-def position_taken?()
-  if condition
-
-  else
-
+  if !won?(board)
+    return nil
+  else WIN_COMBINATIONS.each do |win_combo|
+    if check_win_combination?(board, 'X', win_combo)
+      return 'X'
+    elsif check_win_combination(board, 'O', win_combo)
+      return 'O'
+    end
   end
 end
-
-def valid_move?
-  if condition
-
-  end
-end
-
-def turn()
-
-end
-
-def turn_count(board)
-
 end
 
 def current_player(board)
-
+  if turn_count(board) % 2 == 0
+    current_player = "X"
+  else
+    current_player = "O"
+  end
+  return current_player
 end
 
-def won?(board)
-
+#rb
+def valid_move?(board,index)
+  if index.between?(0,8) && !position_taken?(board,index)
+      puts 'valid move'
+      return true
+  else
+      return false
+  end
 end
 
-def full?(board)
-
+#rb
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+   if space == "X" || space == "O"
+     counter +=1
+   end
+  return counter
 end
 
-def draw?(board)
-
+#rb
+def move(board,index,current_player)
+  board[index] = current_player
+  play(board)
 end
 
-def over?(board)
-
+#rb
+def input_to_index(n)
+  index = n.to_i - 1
+  index
 end
 
-def winner(board)
-
+#rb
+def turn(board)
+  puts 'Please enter 1-9:'
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  # token = current_player(board)
+  if valid_move?(board,index) #### call
+    puts 'valid move!'
+    turn(board)
+  else
+    puts 'not valid, try again'
+    # turn(board)
+  end
+  move(board,index,current_player(board))
+  display_board(board)
 end
 
+#rb
 def play(board)
-
+  until over?(board) == true || won?(board) != false
+    puts 'turn'
+    turn(board)
+  end
+  if winner(board)
+    puts 'Congrats'
+  elsif draw?(board)
+    puts 'Draw!'
+  else
+    return nil
+  end
 end
