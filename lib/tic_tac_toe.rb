920d2f6ce093267aeaa6bc52393b8d08188f6811
Victoria <vitalina27@gmail.com>
2017-12-12 08:12:34 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,172 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]]
 
+
+#OUTPUT BOARD
+def display_board(
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts 'display board'
+#board output
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#CHANGE INFO TO INTEGER
+def input_to_index(move)
+   index = move.to_i - 1
+   index
+end
+
+#MAKE TURN
+def turn(board)
+  puts "Please enter 1-9:"
+  #get the user input
+  user_input = gets.strip
+  #input to index
+  index = input_to_index(user_input)
+  token = current_player(board)
+
+  #check for validation
+  if valid_move?(board,index)
+    puts 'valid move'
+    move(board, index, token)
+    display_board(board)
+   else
+    puts 'try again'
+    turn(board)
+  end
+  display_board(board)
+end
+
+#CHECK FOR EMPTY SPACE
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+#IS # CORRECT & SPACE EMPTY?
+def valid_move?(board, index)
+
+  if index.between?(0,8) && !position_taken?(board, index)
+      puts 'this is a valid move'
+    return true
+  else
+   return false
+  end
+end
+
+#COUNTER FOR PLAYER ASSESMENT
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter +=1
+  end
+end
+return counter
+end
+
+#WHICH PLAYER IS THIS?
+def current_player(board)
+  if turn_count(board)%2 ==0
+    current_player = "X"
+  else
+    current_player = "O"
+end
+return current_player
+end
+
+
+#MAKE MOVE ON BOARD
+def move(board, index, token)
+  board[index] = token
+  play(board)
+
+end
+
+
+
+
+
+#HAS ANYONE WON? return false if no. return win_combo if yes
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    #check for player 1 win
+    if check_win_combination?(board, 'X', win_combo)
+      return win_combo
+      #check for player 2 win
+    elsif check_win_combination?(board, 'O', win_combo)
+      return win_combo
+    else
+      return false
+    end
+  end
+end
+
+#CHECK WINNING COMBOS
+def check_win_combination?(board, player, win_combo)
+  win_combo.all? do |position|
+    board[position] == player
+  end
+end
+
+#IS BOARD FULL?
+def full?(board)
+  if board.include?(' ') || board.include?('')
+    return false
+else
+    return true
+  end
+end
+
+#IS THERE A DRAW?
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  end
+end
+
+#HAS SOMETHING HAPPENED?
+def over?(board)
+  puts 'is it over?'
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    puts 'no keep going'
+    return false
+  end
+end
+
+#WHO WON?
+def winner(board)
+  if !won?(board)
+    return nil
+  else WIN_COMBINATIONS.each do |win_combo|
+    if check_win_combination?(board, 'X', win_combo)
+      return 'X'
+    elsif check_win_combination?(board, 'O', win_combo)
+      return 'O'
+    end
+  end
+end
+end
+def play(board)
+  until over?(board)
+    current_player(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  else
+    puts "Cat's Game!"
+  end
+end
