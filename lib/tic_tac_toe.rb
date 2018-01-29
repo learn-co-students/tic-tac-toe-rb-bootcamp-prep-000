68a4a2b48998365f57effba8a7f7b2d90da9e787
jencekade <gaephuoc@gmail.com>
2018-01-29 00:55:04 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,10 +33,78 @@ def position_taken?(board, position)
   end
 end
 
-def valid_move?(board, postion)
-  if (!position_taken?(board, position))
+def valid_move?(board, position)
+  if (!position_taken?(board, position) && position.between?(0, 8))
     return true
   else
     return false
   end
+end
+=begin
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets
+  index = input_to_index(input)
+  if(valid_move?(board, index))
+    move(board, index)
+    display(board)
+  else
+    puts "That is not a valid selection."
+    turn(board)
+  end
+end
+=end
+def turn_count(board)
+  counter = 0
+  board.each do |location|
+    if (location === "X" || location === "O")
+      counter += 1
+  end
+  counter
+end
+
+def current_player
+
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    firstSpot = combo[0]
+    secondSpot = combo[1]
+    thirdSpot = combo[2]
+    if (board[firstSpot]===board[secondSpot] && board[secondSpot]===board[thirdSpot])
+      return true
+    else
+      return false
+    end
+  combo
+  end
+end
+
+def full?(board)
+  if (board.include?(" "))
+    return false
+  else
+    return true
+  end
+end
+
+def draw?(board)
+  if (!won?(board) && full?(board))
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if (won?(board) || full?(board))
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+
 end
