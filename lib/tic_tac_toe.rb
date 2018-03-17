16143d02e7d4ab618f1fcf3c959fac6a6f412ec1
Ryan O'Toole <otoolerj@gmail.com>
2018-03-17 21:46:23 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,4 @@
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
+require 'pry'
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -33,9 +30,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) || full?(board) || draw?(board)
-    return true
-  end
+  won?(board) || draw?(board) || full?(board)
 end
 
 def winner(board)
@@ -81,12 +76,12 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
+def move(board, index, current_player)
   board[index] = current_player
 end
 
 def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+  board[location] != " " && board[location] != "" && board[location] != nil
 end
 
 def valid_move?(board, index)
@@ -98,9 +93,36 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
+  end
+end
+
+# def play(board)
+#   turn(board)
+#   if over?(board) == false
+#     play(board)
+#   elsif winner?(board) == "X"
+#     puts "Congratulations X"
+#   elsif winner?(board) == "O"
+#     puts "Congratulations O"
+#   else draw?(board) == true
+#     puts "Cat's Game!"
+#   end
+# end
+
+def play(board)
+
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board) && winner(board) == "X"
+    puts "Congratulations X!"
+  elsif won?(board) && winner(board) == "O"
+    puts "Congratulations O!"
+  else
+    puts "Cat's Game!"
   end
 end
