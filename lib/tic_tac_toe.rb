45eb00f40a6f96903da537c74123ad6eaacda9e5
nancy sin <nsin@sbcglobal.net>
2018-03-22 19:31:47 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,7 +5,7 @@
  [6,7,8], #bottom row
  [0,3,6], #left column
  [1,4,7], #middle column
-   [2,5,8], #right column
+ [2,5,8], #right column
   [0,4,8], #diagonal back
   [6,4,2], #diagonal forward
 ]
@@ -18,116 +18,229 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-+def input_to_index(input)
+def input_to_index(input)
+
   input.to_i - 1
+
 end
+
+
 
 def move(board, index, current_player)
+
  board[index] = current_player
+
 end
+
+
 
 def position_taken?(board, location)
+
   board[location] != " " && board[location] != ""
+
 end
+
+
 
 def valid_move?(board, index)
+
  index.between?(0,8) && !position_taken?(board, index)
+
 end
 
+
+
 # def turn(board)
+
    puts "Please enter 1-9:"
+
 #   input = gets.strip
+
 #   index = input_to_index(input)
+
 #   if valid_move?(board, index)
+
 #     move(board, index, current_player)
+
 #     display_board(board)
+
 #   else
+
 #     turn(board)
+
 #   end
+
 # end
 
 
+
+
+
 def turn(board)
+
   puts "Please enter 1-9:"
+
   i = gets.strip
+
   index = input_to_index(i)
+
   m = valid_move?(board, index)
+
   if m == true
+
     move(board, index, current_player(board))
+
  else m == false
+
    until m == true
+
       puts "Sorry, that was an invalid move. Please enter 1-9:"
+
       display_board(board)
+
       i = gets.strip
+
       index = input_to_index(i)
+
       m = valid_move?(board, index)
+
       move(board, index, current_player(board))
+
    end
+
   end
+
 end
+
+
 
 def turn_count(board)
+
  count = 0
+
   board.each do |i|
+
    if i == "X" || i == "O"
+
       count += 1
+
     end
+
   end
+
   count
+
 end
+
+
 
 def current_player(board)
+
  turn_count(board).even? ? "X" : "O"
+
 end
+
+
 
 def won?(board)
+
   WIN_COMBINATIONS.each do |win_combination|
+
     result = []
+
     win_combination.each do |i|
+
      result << board[i]
+
     end
+
    if result == ["X","X","X"] || result == ["O","O","O"]
+
      return win_combination
+
     end
+
  end
+
    return false
+
 end
+
+
 
 def full?(board)
+
   board.all? {|i| i == "X" || i == "O"}
+
 end
+
+
 
 def draw?(board)
+
   (full?(board) && !won?(board)) ? true : false
+
 end
+
+
 
 def over?(board)
+
   (won?(board) || draw?(board)) ? true : false
+
 end
 
+
+
 def winner(board)
+
   if won?(board)
+
     test = won?(board)[0]
+
     # board[test] == "X" ? "X" : "O"
+
    if board[test] == "X"
+
       return "X"
+
     elsif board[test] == "O"
+
       return "O"
+
     end
+
   else
+
     return nil
+
   end
+
 end
+
+
+
 
 
 def play(board)
+
   until over?(board) == true
+
     display_board(board)
+
     turn(board)
+
  end
+
  if draw?(board) == true
+
        puts "Cat's Game!"
+
  else won?(board)
+
     puts "Congratulations #{winner(board)}!"
+
    end
+
+
 end
