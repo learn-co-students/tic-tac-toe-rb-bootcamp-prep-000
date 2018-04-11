f55035ead8ddf8fb109df9e1fbe729b96c571590
John Gratton <jgratton@gmail.com>
2018-04-11 19:42:15 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index) == true
+  if valid_move?(board, index)
     current_player = current_player(board)
     move(board, index, current_player)
     display_board(board)
