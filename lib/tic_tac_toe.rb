0ead50ef7e94db836f3d073d9fc5a618c9d4084f
Adam Mrachek <amrachek@outlook.com>
2018-05-02 13:51:13 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,3 +18,49 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
 end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, player_character)
+  board[index] = player_character
+end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def valid_move?(board, index)
+  !position_taken?(board, index) && (index >= 0 && index <= 9)
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.chomp
+  index = input_to_index(input)
+  character = current_player(board)
+  if valid_move?(board, index)
+    move(board, index, character)
+  else
+    turn(board)
+  end
+end
