31a68aebf3a6669f3b4c92607520747b1233e95d
Adam Mrachek <amrachek@outlook.com>
2018-05-02 22:41:55 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,3 +64,42 @@ def turn(board)
     turn(board)
   end
 end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    position_1 = win_combination[0]
+    position_2 = win_combination[1]
+    position_3 = win_combination[2]
+    if board[position_1] == "X" && board[position_2] == "X" && board[position_3] == "X"
+      return win_combination
+    elsif board[position_1] == "O" && board[position_2] == "O" && board[position_3] == "O"
+      return win_combination
+    end
+  end
+
+  empty_board = board.any? { |position| position == "X" || position == "O" }
+  if empty_board
+    return false
+  end
+end
+
+def full?(board)
+  board.all?{ |position| position == "X" || position == "O"}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  winning_combo = won?(board)
+  if won?(board)
+    return board[winning_combo[0]]
+  else
+    nil
+  end
+end
