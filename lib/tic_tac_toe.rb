025a898c9952e485a696b5c88e5b8665756a3c6f
Luke Worley <luke.worley@colorado.edu>
2018-05-21 22:31:10 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+# Helper Method
+# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -6,20 +8,9 @@ WIN_COMBINATIONS = [
   [1,4,7],
   [2,5,8],
   [0,4,8],
-  [6,4,2]
-]
-
-def play(board)
-  while !over?(board)
-    turn(board)
-  end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cat's Game!"
-  end
-end
-
+  [2,4,6]
+  ]
+  
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -28,65 +19,133 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  return input.to_i - 1
+end
+
+def move(board, position, char="X")
+  board[position]=char
+  return board
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
-  end
-end
-
-def full?(board)
-  board.all?{|token| token == "X" || token == "O"}
-end
-
-def draw?(board)
-  !won?(board) && full?(board)
-end
-
-def over?(board)
-  won?(board) || draw?(board)
-end
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
+  input = gets.strip
+  index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, current_player(board))
+    move(board, index)
     display_board(board)
   else
     turn(board)
   end
+  
 end
 
-def position_taken?(board, index)
-  board[index]== "X" || board[index] == "O"
+# Define your play method below
+def play(board)
+    for i in 0..8
+      if !won?(board)
+        turn(board) 
+      else
+        puts "Congratulations #{winner(board)}"
+      end
+    end
+  
 end
 
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
-def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
-end
-
-def move(board, index, player)
-  board[index] = player
-end
-
-def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
+def full?(board)
+  if board.include?(" ")
+    return false
+  else
+    return true
   end
 end
 
+def over?(board)
+  if draw?(board) || won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board) 
+    return board[won?(board)[0]]
+  else
+    return nil
+  end
+end
+  
+
+  
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def won?(board)
+  #check each of the combos of wins
+  win = true
+  WIN_COMBINATIONS.each do |combo|
+    #puts "combo is #{combo}"
+    # see if all the letters in those indeces are the same letter
+    win = true
+    combo.each do |index|
+      #puts "index being checked is #{index}"
+      #get the winning combos indeces
+      #puts " letter at position is #{board[index]} first position is #{board[combo[0]]}"
+      if board[index] != board[combo[0]] #compare each index with first
+        #puts "lost at 1"
+        win = false
+      end
+      if board[index] == " "
+        #puts "lost at 2"
+        win =  false
+      end
+      
+    end
+    
+    #puts "win is #{win}"
+    
+    if win == true
+      #puts "winning combo is #{combo}"
+      return combo
+    end
+    
+  end
+  #puts "checked all"
+  return win
+end
+
+
+def turn_count(board)
+  count = 0
+  board.each do |turn|
+    if turn != "" && turn != " " && turn != nil
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  if turn_count(board)%2==0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+#won?(["X", "O", "X", "O", "X", "X", "O", "X", "O"])
