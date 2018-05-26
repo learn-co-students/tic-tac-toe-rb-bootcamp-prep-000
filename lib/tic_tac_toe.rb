f1db8fa4d7f8625d34b4db7167333db616c992bb
Valerie Moore <periwinkle17@outlook.com>
2018-05-26 16:30:36 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -88,24 +88,6 @@ def won?(board)
   end
 end
 
-# def won?(board)
-#   WIN_COMBINATIONS.each do |win_combination|
-#     win_index_1 = win_combination[0]
-#     win_index_2 = win_combination[1]
-#     win_index_3 = win_combination[2]
-    
-#     # load the value of the board at each win_index
-#     position_1 = board[win_index_1] # board[0]
-#     position_2 = board[win_index_2] # board[1]
-#     position_3 = board[win_index_3] # board[2]
-#     if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
-#       (position_1 == "O" && position_2 == "O" && position_3 == "O")
-#       return win_combination
-#     end
-#   end
-#   return false
-# end
-
 def full?(board)
   board.all? { |index| index == "X" || index == "O"}
 end
