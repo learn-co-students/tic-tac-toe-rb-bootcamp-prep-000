6a047d4514382080fe5c263f8df0391bd23e658f
Lamido Tijjani <kinglamido@gmail.com>
2018-06-03 02:13:53 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,6 +4,7 @@ WIN_COMBINATIONS = [
   [0, 4, 8], [2, 4, 6]
   ]
 
+
   def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -120,6 +121,7 @@ def winner(board)
 	end
 		}
 end
+
 
 def play(board)
 while !over?(board)
