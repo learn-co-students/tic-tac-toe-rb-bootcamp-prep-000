e42684d560d8bc8a558e7ff39753dd88eaff4857
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-24 21:27:49 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ def input_to_index(user_input)
 end 
 
 
-def move(board, index, current_player = "X")
+def move(board, index, current_player)
   board[index] = current_player
 end
 
@@ -27,6 +27,19 @@ end
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
 
 
