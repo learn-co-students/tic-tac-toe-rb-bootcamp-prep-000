6c493c8ea662249a557acc64028e67ff3142ee28
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-24 21:21:29 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,45 @@
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end 
+
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+
+def turn_count(board)
+  turn_count = board.count("X") + board.count("O")
+end
+
+
+def current_player(board)
+    turn_count(board).even? ? current_player = "X" : current_player = "O"
+end
+
+
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
@@ -18,6 +58,7 @@ def won?(board)
     false
   end
 end
+
 
 def full?(board)
   board.all? do |board_element| 
@@ -52,49 +93,8 @@ def winner(board)
   end 
 end
 
-def turn_count(board)
-  turn_count = board.count("X") + board.count("O")
-end 
+ 
     
-def current_player(board)
-    turn_count(board).even? ? current_player = "X" : current_player = "O"
-end
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
 
 # Define your play method below
 
