cd81469f7896970afb8dd932bba2f9ed1fcf442a
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-24 21:01:15 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,3 +56,11 @@ def winner(board)
     end
   end 
 end
+
+def turn_count(board)
+  turn_count = board.count("X") + board.count("O")
+end 
+    
+def current_player(board)
+    turn_count(board).even? ? current_player = "X" : current_player = "O"
+end
