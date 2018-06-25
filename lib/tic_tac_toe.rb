e2037c8c32037295cf1df33a6e3d2883360fa4fc
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-06-25 06:54:16 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -135,5 +135,11 @@ def play(board)
   until over?(board)
     turn(board)
   end
-end
-
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  else 
+    if draw?(board)
+      puts "Cat's Game!"
+    end 
+  end
+end 
