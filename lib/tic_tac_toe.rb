355633d1b77db5de48f2b543dec4379d4aa3173d
Christine Xiang <yutingcxiang@gmail.com>
2018-06-26 21:53:03 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,76 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(array, index, character)
+  array[index] = character
+end
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && (index >= 0 && index <= 8)
+    true
+  else
+    false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else 
+    true
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  player = current_player(board)
+  if valid_move?(board, index)
+    move(board, index, player)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+# Define your play method below
+def play(board)
+  counter = 1
+  while counter < 10
+    turn(board)
+    counter += 1
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
 
 # Helper Method
 def position_taken?(board, index)
@@ -42,10 +115,6 @@ end
 def draw?(board)
   if won?(board) == false && full?(board) == true
     true
-  elsif won?(board) == false && full?(board) == false
-    false
-  elsif won?(board) == true
-    false
   end
 end
 
