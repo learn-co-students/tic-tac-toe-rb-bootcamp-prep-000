fb9f8d185b7b5ab180cef9620bc5948d853b7193
gmat191@gmail.com <gmat191@gmail.com>
2018-07-03 01:27:53 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,6 +39,18 @@ def valid_move?(board, index)
      board[index] = player
    end
 
+#def turn(board)
+  #puts "Please enter 1-9:"
+  #input = gets.strip
+  #index = input_to_index(input)
+  #if valid_move?(board, index)
+    #move(board, index, character = "X")
+    #display_board(board)
+  #else
+    #turn(board)
+  #end
+#end
+
 def turn(board)
   puts "Please enter 1-9:"    
   input = gets.strip     
