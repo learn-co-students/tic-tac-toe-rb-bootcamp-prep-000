2043929621242a0a51299d53623239f5af8748af
Ben Kudler <bkudler@gmail.com>
2018-08-02 22:00:26 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,74 +1,21 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+Skip to content
 
+Search or jump to…
 
+Pull requests
+Issues
+Marketplace
+Explore
+ @bkudler Sign out
+19
+1 739 learn-co-students/tic-tac-toe-rb-bootcamp-prep-000
+ Code  Issues 0  Pull requests 726  Projects 0  Insights
+tic-tac-toe-rb-bootcamp-prep-000/lib/tic_tac_toe.rb
+7b3dc81  on Aug 24, 2017
+@gj gj Update solution to account for punctuation of Cat's Game
+@aturkewi @aviflombaum @sammarcus @gj @AnnJohn
 
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player)
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  current_player = current_player(board)
-  if valid_move?(board, index)
-    move(board, index, current_player)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-def position_taken?(board, index)
-  board[index] != " "
-end
-
-
-def turn_count(board)
-  # board.count{|token| token == "X" || token == "O"}
-
-  turns = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-      turns += 1
-    end
-  end
-  turns
-
-end
-
-
-
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-# Define your WIN_COMBINATIONS constant
+93 lines (77 sloc)  1.62 KB
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -80,7 +27,29 @@ WIN_COMBINATIONS = [
   [6,4,2]
 ]
 
-# Define won?, full?, draw?, over?, and winner below
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
@@ -94,11 +63,43 @@ def full?(board)
 end
 
 def draw?(board)
-  full?(board) && !won?(board)
+  !won?(board) && full?(board)
 end
 
 def over?(board)
-  won?(board) || full?(board)
+  won?(board) || draw?(board)
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def position_taken?(board, index)
+  board[index]== "X" || board[index] == "O"
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def move(board, index, player)
+  board[index] = player
 end
 
 def winner(board)
@@ -107,20 +108,16 @@ def winner(board)
   end
 end
 
-def play(board)
-  while !over?(board)
-    turn(board)
-    if draw?(board)
-      puts "Cat's Game!"
-    end
-  end
-
-  if draw?(board)
-    puts "Cat's Game!"
-  end
-  
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  end
-
-end
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
+About
+Press h to open a hovercard with more details.
