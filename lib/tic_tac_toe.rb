8815f250a18b63b2af6074d47bace28bf4cc3ca8
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-09-04 17:58:45 -0400

    updates #move method

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index, current_player(board))
+    player_move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
