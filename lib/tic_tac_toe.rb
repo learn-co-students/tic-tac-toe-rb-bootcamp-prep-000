569eb76b872afd64ed3e856e691d3050968ca8b6
Sam B <s77broz@aol.com>
2018-09-07 04:38:27 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ end
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] && board[combo[1]] = board[combo[2]] && position_taken?(board, combo[0])
+    board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
   end
 end
 
