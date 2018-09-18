052416f44aefcb45231f946f8c901f13ace1071c
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-18 18:50:50 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,139 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2],  # row1
+  [3,4,5],  # row2
+  [6,7,8],  # row3
+  [0,3,6],  # column1
+  [1,4,7],  # column2
+  [2,5,8],  # column3
+  [0,4,8],  # diag left to right
+  [2,4,6]   # diag right to left
+]
+
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def player_move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    player_token = current_player(board)
+    player_move(board, index, player_token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |place|
+    if place == "X" || place == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end
+
+
+def won?(board) 
+  WIN_COMBINATIONS.each do |wincombo| 
+    if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X" 
+      return wincombo 
+    elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O" 
+      return wincombo 
+      end 
+  end 
+  false 
+end
+
+
+
+def full?(board)
+  if board.detect {|space| space == " " || space == nil}
+    return false
+  else
+    return true
+  end
+end
+
+
+
+def draw?(board)
+  if !(won?(board)) && full?(board)
+    return true
+  else
+    false
+  end
+end
+
+
+
+def over?(board)
+  if full?(board) || draw?(board) || won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+
+def winner(board)
+  WIN_COMBINATIONS.each do |wincombo| 
+    if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X" 
+      return "X" 
+    elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O" 
+      return "O"
+      end 
+  end 
+  nil 
+end
+
+
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+    if won?(board)
+      puts "Congratulations #{winner(board)}!"
+    elsif draw?(board)
+      puts "Cat's Game!"
+    end
+end
+
+
 
