7f00ae453d4133e5249b1dcd9fb18536ab190af5
maxwellbenton <maxwellbenton@gmail.com>
2018-09-28 08:15:53 -0400

    added solution

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,7 +59,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index, current_player(board))
+    make_move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
@@ -78,7 +78,7 @@ def turn_count(board)
   board.count { |token| token == 'X' || token == 'O' }
 end
 
-def move(board, index, player)
+def make_move(board, index, player)
   board[index] = player
 end
 
