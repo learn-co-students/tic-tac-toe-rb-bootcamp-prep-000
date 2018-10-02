d86bdaa1b0827a24181313452e2cce99ae318575
maxwellbenton <maxwellbenton@gmail.com>
2018-10-02 09:28:39 -0400

    rebase from master

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,11 +59,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-<<<<<<< HEAD
-    make_move(board, index, current_player(board))
-=======
     move(board, index, current_player(board))
->>>>>>> updated solution
     display_board(board)
   else
     turn(board)
