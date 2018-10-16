a99a3ec38a9a676b44fe98bdfbd6a15d9ca438ae
naizal Edy <pritiadhikari@gmail.com>
2018-10-16 01:59:45 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,9 +7,14 @@ def display_board(board)
 end
 
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 
 
-
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
 
 
 def position_taken?(board, index)
@@ -17,31 +22,51 @@ def position_taken?(board, index)
 end
 
 
-  
-counter = 0
-board.each do |turns|
- if turns == "X" || turns == "O"
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+
+def play(board)
+  counter = 0
+  while counter < 9
+  turn (board)
   counter += 1
+  end
 end
-end
-  
-  return counter
+
+
+def turn_count(board)
+  counter = 0
+  board.each do |turns|
+   if turns == "X" || turns == "O"
+   counter += 1
+   end
+ end
+ return counter
 end
 
 
 def current_player(board)
-  
-  turn = turn_count(board)
-  
+   turn = turn_count(board)
   if turn.even?
-    
-    return "X"
-  
-else
-  
-  return "O"
-  
-end
+     return "X"
+  else
+     return "O"
+  end
 end
 
 
@@ -60,24 +85,22 @@ WIN_COMBINATIONS = [
 def won?(board)
   
   WIN_COMBINATIONS.find do |win_combination|
-   win_index_1 = win_combination[0]
+  win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
   
-   position_1 = board[win_index_1]
+  position_1 = board[win_index_1]
   position_2 = board[win_index_2]
   position_3 = board[win_index_3]
   
    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
-  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return win_combination
+     return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+     return win_combination
   else
     false
   end
-end
-
-
+ end
 end
 
 
