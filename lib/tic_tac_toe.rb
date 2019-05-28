872b79d5ca95dc812256f1533a18dc9fa5ec7956
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-28 15:14:57 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,14 +33,83 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
+def turn_count(board)
+  counter = 0 
+  board.each do |index|
+    if index == "X" || index == "O"
+      counter += 1 
+    end   
+  end 
+  return counter
+end 
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end 
+
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
   end
 end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+ 
+  position_1 = board[win_index_1] 
+  position_2 = board[win_index_2] 
+  position_3 = board[win_index_3] 
+ 
+  if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    return win_combination 
+  end
+end
+false
+end
+
+def full?(board)
+  filtered_board = board.reject{|empty_space| empty_space == " "}
+  if filtered_board.length == 9 
+    return true
+  else 
+    false
+  end 
+end 
+
+def draw?(board)
+  !won?(board) && full?(board)
+end 
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do |win_combination|
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+ 
+  position_1 = board[win_index_1] 
+  position_2 = board[win_index_2] 
+  position_3 = board[win_index_3] 
+ 
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return "X" 
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return "O"
+  end
+end
+  nil
+end   
+
+
